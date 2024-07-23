4bdbc438e07acb9dfbe624da0b36c207b5c3123d
Nithin Kumar Jangam <152278019+Nithinkumarjangam@users.noreply.github.com>
2024-07-23 11:45:10 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -6,17 +6,14 @@ print('Loading function')
 
 s3 = boto3.client('s3')
 
-
-def lambda_handler(event,context):
-    #print("Received event:" + json.dumps(events, indent=2))
-    
+def lambda_handler(event, context):
     # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name]
-    key  = urllib.prase.unquote_plus(event['Records'][0]['s3']['object']['key'],encoding='utf-8')
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
-       response =s3.get_object(Bucket=bucket, key=key)
-       print("CONTENT TYPE: " +response['ContentType'])
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this fuction.'.format(Key,bucket))
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
